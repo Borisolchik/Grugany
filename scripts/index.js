e0f487cea815f0adecdb24f5f52f9491c003822b
Olga Boris <olya_boris97@mail.ru>
2024-02-16 20:50:23 +0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,117 +1,133 @@
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i)
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i)
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i)
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i)
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i)
-    },
-    any: function () {
-        return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows());
-    },
-};
-
-if (isMobile.any()) {
-    document.body.classList.add('_touch');
-    let menuArrows = document.querySelectorAll('.menu__link-catalog')
-    if (menuArrows.length > 0) {
-        for (let index = 0; index < menuArrows.length; index++) {
-            const menuArrow = menuArrows[index];
-            menuArrow.addEventListener('click', function (e) {
-                menuArrow.parentElement.classList.toggle('_active');
-            });
-        }
-    }
-
-} else {
-    document.body.classList.add('_pc');
-}
-
-const iconMenu = document.body.querySelector('.menu__icon')
-const iconMenuBlock = document.body.querySelector('.menu__icon-block')
-
-if (iconMenu) {
-    const menuBody = document.body.querySelector('.menu__body')
-    iconMenuBlock.addEventListener('click', function (e) {
-        document.body.classList.toggle('_lock')
-        iconMenu.classList.toggle('_active')
-        menuBody.classList.toggle('_active')
-    })
-}
 $(document).ready(function() {
-    $('.reviews-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 1225, // - от какой ширины изменять настройки(1024 и ниже)
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480, // - от какой ширины изменять настройки(1024 и ниже)
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-        ]
-    });
+    PopUpHide();
 
-    $('.news_items').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-    });
+    const isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i)
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i)
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i)
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i)
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i)
+        },
+        any: function () {
+            return (
+                isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows());
+        },
+    };
 
-    $('.article_items').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-    });
-
-    $('.albums_items').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-    });
-
-    $('.gallery_slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-    });
-
-
-    let array1 = document.querySelectorAll('.trainers-item');
-    for (let i = 3; i < array1.length; i++) {
-        array1[i].classList.add('none');
-    }
-    const button1 = document.querySelector('#trainers_btn');
-    button1.addEventListener('click', () => {
-        let elements1 = document.querySelectorAll('.trainers-item.none');
-
-        for (let i = 0; i < elements1.length; i++) {
-            elements1[i].classList.remove('none');
+    if (isMobile.any()) {
+        document.body.classList.add('_touch');
+        let menuArrows = document.querySelectorAll('.menu__link-catalog')
+        if (menuArrows.length > 0) {
+            for (let index = 0; index < menuArrows.length; index++) {
+                const menuArrow = menuArrows[index];
+                menuArrow.addEventListener('click', function (e) {
+                    menuArrow.parentElement.classList.toggle('_active');
+                });
+            }
         }
-        button1.classList.add('none');
-    });
+
+    } else {
+        document.body.classList.add('_pc');
+    }
+
+    const iconMenu = document.body.querySelector('.menu__icon')
+    const iconMenuBlock = document.body.querySelector('.menu__icon-block')
+
+    if (iconMenu) {
+        const menuBody = document.body.querySelector('.menu__body')
+        iconMenuBlock.addEventListener('click', function (e) {
+            document.body.classList.toggle('_lock')
+            iconMenu.classList.toggle('_active')
+            menuBody.classList.toggle('_active')
+        })
+    }
 
 });
+
+$('.reviews-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 1225, // - от какой ширины изменять настройки(1024 и ниже)
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480, // - от какой ширины изменять настройки(1024 и ниже)
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+    ]
+});
+
+$('.news_items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+});
+
+$('.article_items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+});
+
+$('.albums_items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+});
+
+$('.gallery_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+});
+
+
+let array1 = document.querySelectorAll('.trainers-item');
+for (let i = 3; i < array1.length; i++) {
+    array1[i].classList.add('none');
+}
+const button1 = document.querySelector('#trainers_btn');
+button1.addEventListener('click', () => {
+    let elements1 = document.querySelectorAll('.trainers-item.none');
+
+    for (let i = 0; i < elements1.length; i++) {
+        elements1[i].classList.remove('none');
+    }
+    button1.classList.add('none');
+});
+
+function PopUpShow(){
+    $("#popup-item").show();
+    const menuBody = document.body.querySelector('.menu__body')
+    menuBody.classList.remove('_active');
+}
+function PopUpHide(){
+    $("#popup-item").hide();
+}
+function PopUpHide2(){
+    $("#popup-item-success").hide();
+}
+
